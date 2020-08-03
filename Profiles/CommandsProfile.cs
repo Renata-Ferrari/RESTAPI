c52a187e370c83cc048c9d87e30f7656dc277e03
renata.ferrari@gmail.com <renata.ferrari@gmail.com>
2020-08-03 12:43:01 -0300

    WebAPI - net core

---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using RestAPI.Dtos;
+using RestAPI.Models;
+
+namespace RestAPI.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            CreateMap<Command, CommandUpdateDto>();
+        }
+
+    }
+    
+}
